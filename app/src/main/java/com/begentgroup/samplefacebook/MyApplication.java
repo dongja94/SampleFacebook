1f2e03e87724934b570dcf5c4e74392a4e483b8a
dongja94 <dongja94@gmail.com>
2015-09-18 09:29:48 +0900

    Initial commit

---
+++ app/src/main/java/com/begentgroup/samplefacebook/MyApplication.java
@@ -0,0 +1,21 @@
+package com.begentgroup.samplefacebook;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.facebook.FacebookSdk;
+
+public class MyApplication extends Application {
+	private static Context mContext;
+	
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		mContext = this;
+		FacebookSdk.sdkInitialize(this);		
+	}
+	
+	public static Context getContext() {
+		return mContext;
+	}
+}
